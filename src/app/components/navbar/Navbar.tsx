9ad05d2adbb80607c478fc1aac6761b5d9cdfe99
João Matos <fc56292@alunos.fc.ul.pt>
2024-01-07 14:12:42 +0000

    fixed navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,14 +1,43 @@
-import Link from "next/link";
-import styles from "./Navbar.module.css"; // Adjust the path accordingly
+"use client"
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.css';
 
 export default function NavBar() {
-    return (
-        <div className={styles.topBar}>
-            <ul className={styles.navigation}>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/rooms">Rooms</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-            </ul>
-        </div>
-    );
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className={`${styles.topBar} ${scrolling ? styles.scrolled : ''}`}>
+        <ul className={styles.navigation}>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/rooms">Rooms</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      {scrolling && <div className={styles.placeholder} />}
+    </div>
+  );
 }
