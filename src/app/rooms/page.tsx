011c918ac303d0b42c3830a22a034fdd82d82c9a
João Matos <joao-matos02@hotmail.com>
2024-01-13 22:42:05 +0000

    added room slideshow

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,9 +1,26 @@
-import Link from 'next/link';
+"use client"
 import Footer from '../components/footer/Footer';
 import NavBar from '../components/navbar/Navbar'
 import styles from './rooms.module.css'
 
 export default function Museum() {
+
+    let currentImageIndex = 0;
+    const images = ['/images/40.jpg', '/images/39.jpg', '/images/46.jpg'];
+
+    
+    function changeBackground(roomId:string, direction:number) {
+        const room = document.getElementById(roomId);
+        if (room != null) {
+            currentImageIndex = (currentImageIndex + direction + images.length) % images.length;
+            room.style.opacity = '0.5';
+            setTimeout(() => {
+                room.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+                room.style.opacity = '1';
+            }, 200);
+        }
+    }
+
   return (
     <div>
         <NavBar />
@@ -48,7 +65,10 @@ export default function Museum() {
                     <br/>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
-                <div className={`${styles.room} ${styles.room1}`}/>
+                <div id="room1" className={`${styles.room} ${styles.room1}`}>
+                    <button onClick={() => changeBackground('room1', -1)} className={styles.arrowButton}>←</button>
+                    <button onClick={() => changeBackground('room1', 1)} className={styles.arrowButton}>→</button>
+                </div>
             </div>
 
             <div className={styles.infoWrapper}>
