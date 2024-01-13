776740f01715a6e76e3262d9c5c0cf83d18ba8e3
João Matos <joao-matos02@hotmail.com>
2024-01-13 22:13:51 +0000

    changes on images

--- src/app/museum/page.tsx
+++ src/app/museum/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className={styles.secondDiv}>
 
           <div className={styles.infoWrapper}>
-              <div className={`${styles.photo} ${styles.photo5}`}/>
+              <div className={`${styles.photo} ${styles.photo0}`}/>
               <div className={styles.pricingDiv}>
                 <h1>Pricing</h1>
                 <br/>
@@ -32,7 +32,7 @@ export default function Home() {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <br/>
                     </div>
-                    <div className={`${styles.photo} ${styles.photo6}`}/>
+                    <div className={`${styles.photo} ${styles.photo01}`}/>
                 </div>
                 <div className={styles.secondBackground} />
             </div>
