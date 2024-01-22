c6ce96dd083824d89c72ae6a0f468b2b91d55a64
João Matos <joao-matos02@hotmail.com>
2024-01-22 15:21:49 +0000

    added navbar en dict support

--- src/app/[lang]/components/navbar/Navbar.tsx
+++ src/app/[lang]/components/navbar/Navbar.tsx
@@ -4,7 +4,18 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 import Image from 'next/image';
 
-export default function NavBar({lang, page}: {lang: string, page:string}) {
+export default function NavBar({ lang, page }: { lang: string; page: string }) {
+  const [dictionary, setDictionary] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchDictionary = async () => {
+      const importedDictionary = await import(`@/dictionaries/${lang}.json`);
+      setDictionary(importedDictionary);
+    };
+
+    fetchDictionary();
+  }, [lang]);
+
   const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
@@ -23,25 +34,29 @@ export default function NavBar({lang, page}: {lang: string, page:string}) {
     };
   }, []);
 
+  if (!dictionary) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className={`${styles.topBar} ${scrolling ? styles.scrolled : ''}`}>
         <ul className={styles.navigation}>
           <li>
-            <Link href={`/${lang}`}>About</Link>
+            <Link href={`/${lang}`}>{dictionary.navbar.about}</Link>
           </li>
           <li>
-            <Link href={`/${lang}/rooms/`}>Rooms</Link>
+            <Link href={`/${lang}/rooms/`}>{dictionary.navbar.rooms}</Link>
           </li>
           <li>
-            <Link href={`/${lang}/museum/`}>Museum</Link>
+            <Link href={`/${lang}/museum/`}>{dictionary.navbar.museum}</Link>
           </li>
         </ul>
         <Image
           src="/images/Rural_Alberto_Logo.png"
           alt="Logo"
-          layout='fill'
-          objectFit='contain'
+          layout="fill"
+          objectFit="contain"
           style={{ padding: '1em', zIndex: '-1' }}
         />
         <ul className={styles.navigation}>
